4ae60c0a75874e6ec27f6aab8895c6fb6e622007
valjacques Nyemb <nyvanyemb@gmail.com>
2023-06-21 03:40:45 +0100

    add the other files

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,15 +5,7 @@
  * @c: the character to check
  * Return: 1 if c is lowercase, 0 otherwise
  */
-
 int _islower(int c)
 {
-	int num;
-
-	num = _putchar(c + '0');
-	if ((num >= 97) && (num < 123))
-		return (1);
-	else
-		return (0);
+	return (c >= 'a' && c <= 'z');
 }
-
