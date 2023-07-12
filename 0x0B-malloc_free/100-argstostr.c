a51af3b043609bd7ccf12bcefecb7b320acbe7c2
valjacques Nyemb <nyvanyemb@gmail.com>
2023-07-12 22:52:27 +0100

    add 100-argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,45 +1,43 @@
+#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * *argstostr - concatenates all the arguments of the program
- * @ac: number of arguments
- * @av: array of arguments
- *
- * Return: Pointer to the new string (Success), NULL (Error)
+ * argstostr - concatenates the argumnets of program
+ * @ac: number of argumnets
+ * @av: pointer to the arguments array
+ * Return: pointer to concatenated arguements
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len;
-	char *str;
+char *ptr;
+int index = 0, i, j, count = 0;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
-	}
-
-	str = malloc(sizeof(char) * (len + 1));
-
-	if (str == NULL)
-		return (NULL);
-
-	k = 0;
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			str[k] = av[i][j];
-			k++;
-		}
-		str[k] = '\n';
-		k++;
-	}
-
-	return (str);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+count++;
 }
+count++;
+}
+
+ptr = malloc((count + 1) * (sizeof(char)));
+
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+ptr[index++] = av[i][j];
+}
+if (av[i][j] == '\0')
+{
+ptr[index++] = '\n';
+}
+}
+
+ptr[index + 1] = '\n';
+
+return (ptr);
+}
+
